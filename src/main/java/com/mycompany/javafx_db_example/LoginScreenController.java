bbaa6265197d1b169ec79e04f6b084c81032befa
catamv3 <catamv3@farmingdale.edu>
2023-10-25 00:06:57 -0400

    adding login view style

--- src/main/java/com/mycompany/javafx_db_example/LoginScreenController.java
+++ src/main/java/com/mycompany/javafx_db_example/LoginScreenController.java
@@ -1,8 +1,15 @@
 package com.mycompany.javafx_db_example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginScreenController {
     @FXML
@@ -10,12 +17,26 @@ public class LoginScreenController {
 
     @FXML
     private TextField uNameTxtfld, pwTxtfld;
-    public void initialize(){
 
+
+    @FXML
+    public void loginClicked(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("primary.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 850, 560);
+        scene.getStylesheets().add("sunset.css");
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void loginClicked(){
-
+    @FXML
+    public void createAccountClicked(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createAccountScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 850, 560);
+        scene.getStylesheets().add("sunset.css");
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
